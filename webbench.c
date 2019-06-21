9d39fea88f4ee429d58bf32c17e2f9c892724d3f
Starry <1173298382@qq.com>
2019-06-21 23:42:01 +0800

    add fun

--- webbench.c
+++ webbench.c
@@ -1,2 +1,17 @@
+#include "socket.c"
 #include <unistd.h>
 
+
+
+
+static void alarm_handler(int signal);
+static void usage(void);
+void build_request(void);
+static int bench(void);
+void benchcore(const char *host, const int port, const char *req);
+
+int main() {
+
+
+    return 0;
+}
