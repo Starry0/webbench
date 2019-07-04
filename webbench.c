6c679329c69465f4b376e9fdb47cf1df3a95042f
Starry <1173298382@qq.com>
2019-07-04 23:22:07 +0800

    2019-7-4

--- webbench.c
+++ webbench.c
@@ -3,7 +3,7 @@
 #include <sys/param.h>
 #include <rpc/types.h>
 #include <getopt.h>
-#include <string.h>
+#include <strings.h>
 #include <time.h>
 #include <signal.h>
 #include <stdio.h>
@@ -11,7 +11,7 @@
 /*values*/
 volatile int timerexpired = 0;//是否达到指定时间
 int speed = 0;  //记录服务器响应的数量
-int falied = 0; //失败的数量
+int failed = 0; //失败的数量
 int bytes = 0;  //读取成功的字节数
 
 /*globals*/
@@ -37,21 +37,22 @@ char request[REQUEST_SIZE];
 
 static const struct option long_options[]=
 {
- {"force",no_argument,&force,1},
- {"reload",no_argument,&force_reload,1},
- {"time",required_argument,NULL,'t'},
- {"help",no_argument,NULL,'?'},
- {"http09",no_argument,NULL,'9'},
- {"http10",no_argument,NULL,'1'},
- {"http11",no_argument,NULL,'2'},
- {"get",no_argument,&method,METHOD_GET},
- {"head",no_argument,&method,METHOD_HEAD},
- {"options",no_argument,&method,METHOD_OPTIONS},
- {"trace",no_argument,&method,METHOD_TRACE},
- {"version",no_argument,NULL,'V'},
- {"proxy",required_argument,NULL,'p'},
- {"clients",required_argument,NULL,'c'},
- {NULL,0,NULL,0}
+
+    {"force",no_argument,&force,1},
+    {"reload",no_argument,&force_reload,1},
+    {"time",required_argument,NULL,'t'},
+    {"help",no_argument,NULL,'?'},
+    {"http09",no_argument,NULL,'9'},
+    {"http10",no_argument,NULL,'1'},
+    {"http11",no_argument,NULL,'2'},
+    {"get",no_argument,&method,METHOD_GET},
+    {"head",no_argument,&method,METHOD_HEAD},
+    {"options",no_argument,&method,METHOD_OPTIONS},
+    {"trace",no_argument,&method,METHOD_TRACE},
+    {"version",no_argument,NULL,'V'},
+    {"proxy",required_argument,NULL,'p'},
+    {"clients",required_argument,NULL,'c'},
+    {NULL,0,NULL,0}
 };
 
 static void alarm_handler(int signal) {
@@ -76,7 +77,7 @@ static void usage(void) {
 	"  -V|--version             Display program version.\n"
 	);
 }
-void build_request(void);
+void build_request(const char* url);
 static int bench(void);
 void benchcore(const char *host, const int port, const char *req);
 
@@ -106,11 +107,213 @@ int main(int argc, char *argv[]) {
                     fprintf(stderr,"Error in option --proxy %s: Missing hostname.\n",optarg);
 		            return 2;
                 }
-                
+                if(tmp == optarg+strlen(optarg)-1) {
+                    fprintf(stderr,"Error in option --proxy %s Port number is missing.\n",optarg);
+                    return 2;
+                }
+                *tmp = '\0';
+                proxyport = atoi(tmp+1);
+                break;
             } 
+            case ':':
+            case 'h':
+            case '?': usage(); return 2;break;
+            case 'c': clients = atoi(optarg);break;
         }
     }
-    
+    if(optind == argc) {
+        fprintf(stderr,"webbench: Missing URL!\n");
+        usage();
+        return 2;
+    }
+    if(clients == 0) clients = 1;
+    if(benchtime == 0) benchtime = 60;
+    build_request(argv[optind]);
+    fprintf(stderr,"Webbench - Simple Web Benchmark "PROGRAM_VERSION"\n"
+	 "Copyright (c) Radim Kolar 1997-2004, GPL Open Source Software.\n"
+	);
+    printf("\nBenchmarking: ");
+    switch(method) {
+        case METHOD_GET:
+        default:
+            printf("Get");break;
+        case METHOD_HEAD:
+            printf("HEAD");break;
+        case METHOD_OPTIONS:
+            printf("OPTION");break;
+        case METHOD_TRACE:
+            printf("TRACE");break;
+    }
+    printf(" %s",argv[optind]);
+    switch(http10) {
+        case 0: printf(" (using HTTP/0.9)");break;
+        case 2: printf(" (using HTTP/1.1)");break;
+    }
+    printf("\n");
+    if(clients == 1) printf("1 clien");
+    else printf("%d clients",clients);
+    printf(", running %d sec",benchtime);
+    if(force) printf(". early socket close");
+    if(proxyhost != NULL) printf(", via proxy server %s:%d",proxyhost,proxyport);
+    if(force_reload) printf(", forcing reload");
+    printf(".\n");
+    return bench();
+}
 
-    return 0;
+void build_request(const char* url) {
+    char tmp[10];
+    int i;
+    bzero(host, MAXHOSTNAMELEN);
+    bzero(request, REQUEST_SIZE);
+
+    if(force_reload && proxyhost != NULL && http10 < 1) http10 = 1;
+    if(method == METHOD_HEAD && http10 < 1) http10 = 1;
+    if(method == METHOD_OPTIONS && http10 < 2) http10 = 2;
+    if(method == METHOD_OPTIONS && http10 < 2) http10 = 2;
+
+    switch(method) {
+        default:
+        case METHOD_GET: strcpy(request,"GET");break;
+        case METHOD_HEAD: strcpy(request, "HEAD");break;
+        case METHOD_OPTIONS: strcpy(request, "OPTIONS");break;
+        case METHOD_TRACE: strcpy(request, "TRACE");break;
+    }
+
+    strcat(request, " ");
+
+    if(strstr(url, "://") == NULL) {
+        fprintf(stderr, "\n %s: is not a valid URL.\n",url);
+        exit(2);
+    }
+    if(strlen(url) > 1500) {
+        fprintf(stderr, "URL is too long.\n");
+        exit(2);
+    }
+    if(proxyhost == NULL) {
+        if(strncasecmp("http://",url,7) != NULL) {
+            fprintf(stderr,"\nOnly HTTP protocol is directly supported, set --proxy for others.\n");
+            exit(2);
+        }
+    }
+
+    i = strstr(url, "://") - url + 3;
+    // printf("%s %d\n",url+i,i);
+
+    if(strchr(url+i, '/') == NULL) {
+        fprintf(stderr,"\nInvalid URL syntax - hostname don't ends with '/'.\n");
+        exit(2);
+    }
+    if(proxyhost == NULL) {
+        /** get port from hostname */
+        if(strchr(url+i, ':') != NULL && strchr(url+i, ':') < strchr(url+i, '/')) {
+            strncpy(host, url+i, strchr(url+i,':')-url-i);
+            bzero(tmp, 10);
+            strncpy(tmp, strchr(url+i,':')+1,strchr(url+i,'/')-strchr(url+i,':')-1);
+            // printf("%s %s\n",host, tmp);
+            proxyport = atoi(tmp);
+            if(proxyport == NULL) proxyport = 80;
+        } else {
+            strncpy(host, url+i, strcspn(url+i, '/'));
+        }
+        // printf("Host=%s\n",host);
+        strcat(request+strlen(request), url+i+strcspn(url+i, "/"));
+        // printf("request=%s\n",request);
+    } else {
+        strcat(request, url);
+    }
+    if(http10 == 1) strcat(request, " HTTP/1.0");
+    else if(http10 == 2) strcat(request, "HTTP/1.1");
+    strcat(request, "\r\n");
+    if(http10 > 0)
+        strcat(request, "User-Agent: WebBench "PROGRAM_VERSION"\r\n");
+    if(proxyhost == NULL && http10 > 0) {
+        strcat(request, "Host: ");
+        strcat(request,host);
+        strcat(request, "\r\n");
+    }
+    if(force_reload && proxyhost != NULL) {
+        strcat(request, "Pragma: no-cache\r\n");
+    }
+    if(http10 > 1) {
+        strcat(request, "Connection: close\r\n");
+    }
+    if(http10 > 0) strcat(request, "\r\n");
+    printf("request=%s\n",request);
+}
+
+static int bench() {
+    int i, j, k;
+    pid_t pid = 0;
+    FILE *file;
+    printf("dsadsa\n");
+    i = Socket(proxyhost==NULL?host:proxyhost, proxyport);
+    if(i < 0) {
+        fprintf(stderr,"\nConnect to server failed. Aborting benchmark.\n");
+        return 1;
+    }
+    close(i);
+    if(pipe(mypipe)) {
+        perror("pipe failed.");
+        return 3;
+    }
+
+    for(i = 0; i < clients; i ++) {
+        pid = fork();
+        if(pid <= (pid_t)0) {
+            sleep(1);
+            break;
+        }
+    }
+    if(pid < (pid_t)0) {
+        fprintf(stderr,"problems forking worker no. %d\n",i);
+        perror("fork failed.");
+        return 3;
+    }
+
+    if(pid == (pid_t)0) {
+        // printf("%d client",pid);
+        /** in child */
+        benchcore(proxyhost==NULL?host:proxyhost, proxyport, request);
+        file = fdopen(mypipe[1], "w");
+        if(file == NULL) {
+            perror("open pipe for writing failed.");
+            return 3;
+        }
+        fprintf(file, "%d %d %d\n",speed+1, failed+1, bytes+1);
+        fclose(file);
+        // printf("%d client",pid);
+        return 0;
+    } else {
+        file = fdopen(mypipe[0], "r");
+        if(file == NULL) {
+            perror("open pipe for reading failed.");
+            return 3;
+        }
+        setvbuf(file, NULL, _IONBF, 0);
+        speed = failed = bytes = 0;
+        while(1) {
+            pid = fscanf(file, "%d %d %d", &i, &j, &k);
+            if(pid < 2) {
+                fprintf(stderr,"Some of our childrens died.\n");
+                break;
+            }
+            speed += i;
+            failed += j;
+            bytes += k;
+            if(--clients == 0) break;
+        }
+        fclose(file);
+        printf("\nSpeed=%d pages/min, %d bytes/sec.\nRequests: %d susceed, %d failed.\n",
+		    (int)((speed+failed)/(benchtime/60.0f)),
+		    (int)(bytes/(float)benchtime),
+		    speed,failed);
+
+    }
+    return i;
+}
+
+void benchcore(const char *host, const int port, const char *req) {
+    int rlen;
+    char buf[1500];
+
 }
